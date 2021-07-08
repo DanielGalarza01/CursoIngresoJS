498f89a07009e79379acb2e15b9d780f2575a1c4
DanielGalarza01 <galarzadanieleduardo@gmail.com>
2021-07-08 08:15:04 -0300

    tp1

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+
+//definimos las variables.
+	let importe;
+	let descuento;
+	let resultado;
+
+//Asignamos valores a las variables.
+	importe = parseInt(document.getElementById("txtIdImporte").value);
+	descuento = importe * 0.25;
+	resultado = importe - descuento;
+
+//mostramos el resultado.
+document.getElementById("txtIdResultado").value = resultado;
+
 }
--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,62 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+     
+//Definimos las variables.
+    let precioUno;
+    let precioDos;
+    let precioTres;
+    let resultado;
+
+//Asignamos valores a las variables.
+    precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+    precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+    precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+    resultado = precioUno + precioDos + precioTres;
+
+//Devolvemos el valor requerido.
+    alert("El resultado de la suma es "+ resultado);
+
 }
+
 function Promedio () 
+
 {
-	
+     
+//Definimos las variables.
+let precioUno;
+let precioDos;
+let precioTres;
+let resultado;
+
+//Asignamos valores a las variables.
+precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+resultado = (precioUno + precioDos + precioTres)/3;
+
+//Devolvemos el valor requerido.
+alert("El promedio es "+ resultado);
+
 }
+
 function PrecioFinal () 
+
 {
-	
+     
+//Definimos las variables.
+let precioUno;
+let precioDos;
+let precioTres;
+let resultado;
+
+//Asignamos valores a las variables.
+precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+resultado = (precioUno + precioDos + precioTres) * 1.21;
+
+//Devolvemos el valor requerido.
+alert("El precio final es "+ resultado);
+
 }
